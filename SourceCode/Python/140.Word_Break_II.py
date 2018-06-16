5255b770c9d93150d52488bdd285bc7a04fd3470
roger6blog <nospam@roger6.cu.cc>
2018-06-16 13:03:59 +0800

    Update 140.Word_Break_II.py

--- SourceCode/Python/140.Word_Break_II.py
+++ SourceCode/Python/140.Word_Break_II.py
@@ -50,11 +50,15 @@ class Solution(object):
         self.ans = []
         self.dfs(s, wordDict, '')
         return self.ans
+      
     def isWordBreak(self, s, wordDict):
+        # length of dp is 0, it means the previous element was handle, we can continue to parse from 0 now
         dp = [False for i in xrange(len(s)+1)]
         dp[0] = True
 
         for i in xrange(len(s)):
+            # All valid word break from beginning. We cannot process behind string if we don't process front string
+            # for loop: initial value =i, final value = -1, intervel = -1            
             for j in xrange(i, -1, -1):
                 if dp[j] and s[j:i+1] in wordDict:
                     dp[i+1] = True
@@ -62,14 +66,20 @@ class Solution(object):
         return dp[len(s)]
 
     def dfs(self, s, wordDict, stringlist):
+        # Check if the word can be break into word in dictionary
+        # We call this to reduce space requirement of DFS
         if self.isWordBreak(s, wordDict):
             if len(s) == 0:
+                # Trace to end side, so process is complete
                 self.ans.append(stringlist[1:])
 
             for i in xrange(1, len(s)+1):
+                # If left side of word in dictionary
                 if s[:i] in wordDict:
                     # Leetcode will occur Memory Exceed problem for this statement
                     #stringlist = ''.join([stringlist, ' ', s[:i]])
+                    
+                    # Add right side to check word break in DFS
                     self.dfs(s[i:], wordDict, stringlist+' '+s[:i])
             
 sol = Solution()
