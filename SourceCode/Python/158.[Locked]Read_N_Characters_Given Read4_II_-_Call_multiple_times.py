86a52aa1337bf6f01a27213619a4042f67cfb30d
roger6blog <nospam@roger6.cu.cc>
2018-06-19 04:46:36 +0800

    Update 158.[Locked]Read_N_Characters_Given Read4_II_-_Call_multiple_times.py

--- SourceCode/Python/158.[Locked]Read_N_Characters_Given Read4_II_-_Call_multiple_times.py
+++ SourceCode/Python/158.[Locked]Read_N_Characters_Given Read4_II_-_Call_multiple_times.py
@@ -22,6 +22,44 @@ class Solution(object):
 
     def read(self, buf, n):
         count, reads = 0, [''] * 4
-        
+
         if self.left:
-        
+            while count < min(len(self.left), n):
+                buf[count] = self.left[count]
+                count += 1
+            self.left = self.left[count:]
+
+        while count < n:
+            size = read4(reads)
+            if not size:
+                break
+            surplus = min(n - count, size)
+            self.left.append(reads[surplus:size])
+            #self.left = reads[surplus:size]
+            #for i in xrange(surplus):
+            for i in xrange(size - surplus):
+                buf[count] = reads[i]
+                count += 1
+        return count
+
+    
+index = 0
+string = 'abcdefg'
+
+def read4(reads):
+    global index
+
+    count = 0
+    while count < 4 and index < len(string):
+        reads[count] = string[index]
+        count += 1
+        index += 1
+    return count
+
+
+s = Solution()
+buf = [0] * 100
+print s.read(buf, 3)
+print s.read(buf, 4)
+print s.read(buf, 3)
+            
