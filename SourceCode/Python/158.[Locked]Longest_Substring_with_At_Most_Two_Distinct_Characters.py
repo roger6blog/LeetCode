95910aace714126caf6dca8510f590c94f892fa3
roger6blog <nospam@roger6.cu.cc>
2018-06-19 23:57:26 +0800

    Create 158.[Locked]Longest_Substring_with_At_Most_Two_Distinct_Characters.py

---
+++ SourceCode/Python/158.[Locked]Longest_Substring_with_At_Most_Two_Distinct_Characters.py
@@ -0,0 +1,14 @@
+
+
+'''
+Level: Hard
+
+Given a string S, find the length of the longest substring T that contains at most two distinct characters.
+For example,
+Given S = “eceba”,
+T is “ece” which its length is 3.
+
+'''
+
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s):
