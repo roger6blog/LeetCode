2dd0125a33feacdf522b830e4d3161ecc512eb3c
roger6blog <nospam@roger6.cu.cc>
2018-06-20 12:50:18 +0800

    Update 253.Meeting_Rooms_II.py

--- SourceCode/Python/253.Meeting_Rooms_II.py
+++ SourceCode/Python/253.Meeting_Rooms_II.py
@@ -8,12 +8,43 @@ Given [[0, 30],[5, 10],[15, 20]],
 return 2.
 
 '''
-
 # Definition for an interval.
 class Interval(object):
     def __init__(self, s=0, e=0):
         self.start = s
         self.end = e
 
+
 class Solution:
     def minMeetingRooms(self, intervals):
+        if intervals == [] or len(intervals) == 0:
+            return 0
+
+        tmp = []
+
+        for i in intervals:
+            tmp.append([i.start, True])
+            tmp.append([i.end, False])
+
+        tmp.sort(key=lambda x: x[0])
+
+        maxNum = 0
+        currNum = 0
+
+        for node, isStart in tmp:
+            if isStart:
+                currNum += 1
+            else:
+                currNum -= 1
+            maxNum = max(maxNum, currNum)
+        return maxNum
+
+    
+a = Interval(0, 30)
+b = Interval(5, 10)
+c = Interval(15, 20)
+s = [a,b,c]
+sol = Solution()
+print sol.minMeetingRooms(s)
+        
+        
