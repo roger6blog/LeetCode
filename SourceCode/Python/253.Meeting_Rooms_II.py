44641287ab38b766b7ed5891ef7aa5aafb9eb7f0
roger6blog <nospam@roger6.cu.cc>
2018-06-20 12:34:38 +0800

    Create 253.Meeting_Rooms_II.py

---
+++ SourceCode/Python/253.Meeting_Rooms_II.py
@@ -0,0 +1,19 @@
+
+'''
+Level: Hard Tag:[List]
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return 2.
+
+'''
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+class Solution:
+    def minMeetingRooms(self, intervals):
