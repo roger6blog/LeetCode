36a67aaef551e3ac2c9cf3d0e05829938297cfd0
Masato <nospam@roger6.cu.cc>
2018-07-14 15:52:30 +0800

    239.Sliding_Window_Maximum
    Signed-off-by: Masato <nospam@roger6.cu.cc>

---
+++ SourceCode/Python/239.Sliding_Window_Maximum.py
@@ -0,0 +1,67 @@
+'''
+
+Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+
+Example:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Note:
+You may assume k is always valid,
+1 <= k <= input array's size for non-empty array
+
+Follow up:
+Could you solve it in linear time?
+
+
+'''
+
+
+class Solution(object):
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+        if length == 0:
+            return []
+        ans = []
+        for i in xrange(length):
+            if i+k <= length:
+                ans.append(max(nums[i:i+k]))
+        return ans
+
+import collections
+class DoubleQueueSolution:
+    # @param {integer[]} nums
+    # @param {integer} k
+    # @return {integer[]}
+    def maxSlidingWindow(self, nums, k):
+        dq = collections.deque()
+        ans = []
+        for i in range(len(nums)):
+            while dq and nums[dq[-1]] <= nums[i]:
+                dq.pop()
+            dq.append(i)
+            if dq[0] == i - k:
+                dq.popleft()
+            if i >= k - 1:
+                ans.append(nums[dq[0]])
+        return ans
+
+nums = [1,3,-1,-3,5,3,6,7]
+k = 3
+print DoubleQueueSolution().maxSlidingWindow(nums, k)
