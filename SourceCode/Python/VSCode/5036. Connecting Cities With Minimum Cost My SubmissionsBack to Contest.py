a244bb844b8d2e84b2888539822d900300d8e4fc
roger6 <roger6@blogspot.com>
2019-07-28 10:11:33 +0800

    nothing

--- SourceCode/Python/VSCode/5036. Connecting Cities With Minimum Cost My SubmissionsBack to Contest.py
+++ SourceCode/Python/VSCode/5036. Connecting Cities With Minimum Cost My SubmissionsBack to Contest.py
@@ -8,7 +8,7 @@ You are given conections, where each conections[i] = [city1, city2, cost] repres
 
 Return the minimum cost so that for every pair of cities, there exists a path of connections (possibly of length 1) that connects those two cities together.  The cost is the sum of the connection costs used. If the task is impossible, return -1.
 
- 
+
 
 Example 1:
 
@@ -16,7 +16,7 @@ Example 1:
 
 Input: N = 3, conections = [[1,2,5],[1,3,6],[2,3,1]]
 Output: 6
-Explanation: 
+Explanation:
 Choosing any 2 edges will connect all cities so we choose the minimum 2.
 Example 2:
 
@@ -24,9 +24,9 @@ Example 2:
 
 Input: N = 4, conections = [[1,2,3],[3,4,4]]
 Output: -1
-Explanation: 
+Explanation:
 There is no way to connect all cities even if all edges are used.
- 
+
 
 Note:
 
@@ -38,43 +38,53 @@ conections[i][0] != conections[i][1]
 
 
 '''
+
+import operator
+
+
 class edge(object):
     def __init__(self, u, v, c):
         self.u = u
         self.v = v
         self.c = c
 
+    def __lt__(self, other):
+        return self.c < other.c
+
+
 class Solution(object):
+    def find(self, num):
+        if self.pre[num] == num:
+            return num
+
+        self.pre[num] = self.find(num)
+        return self.pre[num]
+
+    def merge(self, u, v):
+        f1 = self.find(u)
+        f2 = self.find(v)
+        if f1 != f2:
+            self.pre[f2] = f1
+            return 1
+        return 0
+
     def minimumCost(self, N, conections):
         """
         :type N: int
         :type conections: List[List[int]]
         :rtype: int
         """
+        self.pre = [-1] * 10040
 
 
-        def find(num, pre):
-            if pre[num] == num:
-                return num
 
-            pre[num] = find(num, pre)
-            return pre[num]
 
-        def merge(u, v, pre):
-            f1 = find(u, pre)
-            f2 = find(v, pre)
-            if f1 != f2:
-                pre[f2] = f1
-                return 1
-            return 0
-        
-        pre = [-1] * 10040
-        e = [edge(0,0,0)] * 10040
-        # for _ in range(10040):
-        #     x = edge()
-        #     e.append(x)
+        e = []
+        # e = [edge(0 ,0 ,0)] * 10040
+        for _ in range(10040):
+            e.append(edge(0, 0, 0))
         for i in range(N):
-            pre[i] = i
+            self.pre[i] = i
 
         n = len(conections)
 
@@ -83,7 +93,17 @@ class Solution(object):
             e[i].v = conections[i][1]
             e[i].c = conections[i][2]
 
-        # e = sorted(e, e)    
-        pass
+        # e.sort()
+        sorted_e = sorted(e[:n], key=operator.attrgetter('c'), reverse=False)
+        ans = 0
+        cnt = 0
+        for i in range(n):
+            if self.merge(sorted_e[i].u ,sorted_e[i].v):
+                cnt += 1
+                ans += sorted_e[i].c
+                if cnt == N-1:
+                    return ans
+
+        return -1
 
 print Solution().minimumCost(3, [[1,2,5],[1,3,6],[2,3,1]])
