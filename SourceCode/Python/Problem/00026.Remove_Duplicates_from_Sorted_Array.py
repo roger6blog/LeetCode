089a5a5c50546b663a43cf2f740449cd9202336d
roger <roger@macos.com>
2021-12-05 14:36:31 +0800

    change title description

--- SourceCode/Python/Problem/00026.Remove_Duplicates_from_Sorted_Array.py
+++ SourceCode/Python/Problem/00026.Remove_Duplicates_from_Sorted_Array.py
@@ -1,44 +1,39 @@
 
 '''
-Given a sorted array nums,
-remove the duplicates in-place such that each element appear only once and return the new length.
+Given an integer array nums sorted in non-decreasing order,
+remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same.
 
-Do not allocate extra space for another array,
-you must do this by modifying the input array in-place with O(1) extra memory.
+Since it is impossible to change the length of the array in some languages,
+you must instead have the result be placed in the first part of the array nums.
+More formally, if there are k elements after removing the duplicates,
+then the first k elements of nums should hold the final result.
+It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
 
 Example 1:
 
-Given nums = [1,1,2],
-
-Your function should return length = 2,
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2,
 with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
 
-It doesn't matter what you leave beyond the returned length.
 Example 2:
 
-Given nums = [0,0,1,1,1,2,2,3,3,4],
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5,
+with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
 
-Your function should return length = 5,
-with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
 
-It doesn't matter what values are set beyond the returned length.
-Clarification:
+Constraints:
 
-Confused why the returned value is an integer but your answer is an array?
-
-Note that the input array is passed in by reference,
-which means modification to the input array will be known to the caller as well.
-
-Internally you can think of this:
-
-// nums is passed in by reference. (i.e., without making a copy)
-int len = removeDuplicates(nums);
-
-// any modification to nums in your function would be known by the caller.
-// using the length returned by your function, it prints the first len elements.
-for (int i = 0; i < len; i++) {
-    print(nums[i]);
-}
+0 <= nums.length <= 3 * 104
+-100 <= nums[i] <= 100
+nums is sorted in non-decreasing order.
 
 
 '''
@@ -50,7 +45,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums: 
+        if not nums:
             return 0
         i = 0
         for j in xrange(1, len(nums)):
