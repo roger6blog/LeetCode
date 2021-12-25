3e06dc9a4e63bf58ac97a951066f69b1dc9e1b25
roger <roger@macos.com>
2021-12-25 15:10:58 +0800

    Add topics

--- SourceCode/Python/Problem/00026.Remove_Duplicates_from_Sorted_Array.py
+++ SourceCode/Python/Problem/00026.Remove_Duplicates_from_Sorted_Array.py
@@ -48,11 +48,12 @@ class Solution(object):
         if not nums:
             return 0
         i = 0
-        for j in xrange(1, len(nums)):
+        for j in range(1, len(nums)):
             if nums[i] != nums[j]:
                 i += 1
                 nums[i] = nums[j]
         return i+1
 
 nums = [0,0,1,1,1,2,2,3,3,4]
-print Solution().removeDuplicates(nums)
+
+print(Solution().removeDuplicates(nums))
