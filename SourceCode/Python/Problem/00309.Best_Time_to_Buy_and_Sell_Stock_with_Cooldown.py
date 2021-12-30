5bdbc3f4b39b91e23919d51e856e4d1f140b90e7
roger6 <roger6@blogspot.com>
2021-12-30 21:22:19 +0800

    add topics

--- SourceCode/Python/Problem/00309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
+++ SourceCode/Python/Problem/00309.Best_Time_to_Buy_and_Sell_Stock_with_Cooldown.py
@@ -1,5 +1,5 @@
 '''
-Level: Medium Tags:
+Level: Medium Tags:[DP]
 Say you have an array for which the i(th) element is the price of a given stock on day i.
 
 Design an algorithm to find the maximum profit.
@@ -50,5 +50,49 @@ class Solution(object):
         return sell[length-1]
 
 
+class Solution2(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        '''
+        There are 4 actions we can do at one day:
+        1. has 0 stock, and buy it
+        2. has 1 stock, but do nothing
+        3. has 0 stock, then do nothing
+        4. has 1 stock, then sell it
+
+        If you choose action 1 at i day, what action you can do is 3 at i-1 day
+        If you choose action 2 at i day, what action you can do is 1 or 2 at i-1 day
+        If you choose action 3 at i day, what action you can do is 2 or 3 at i-1 day
+        If you choose action 4 at i day, what action you can do is 1 or 4 at i-1 day
+        '''
+
+
+        if len(prices) < 2:
+            return 0
+
+        has0_buy_1 = -prices[0]
+        has1_do_nothing = -prices[0]
+        has0_do_nothing = 0
+        has1_sell_1 = 0
+
+
+
+        for i in range(len(prices)):
+            has0_buy_1 = -prices[i] + has0_do_nothing
+            has1_do_nothing = max(has1_do_nothing, has0_buy_1)
+            has0_do_nothing = max(has1_sell_1, has0_do_nothing)
+            has1_sell_1 = prices[i] + has1_do_nothing
+
+        return max(has1_sell_1, has0_do_nothing)
+
+
+
+
+
 stock = [1,2,3,0,2]
-print Solution().maxProfit(stock)
+print(Solution().maxProfit(stock))
+stock = [1,2,3,0,2]
+print(Solution2().maxProfit(stock))
