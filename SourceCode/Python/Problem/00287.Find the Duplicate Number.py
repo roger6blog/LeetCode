302118389d4dd4886a6571903f45bede4642480d
roger <roger@macos.com>
2021-12-31 12:42:13 +0800

    Modify 287

--- SourceCode/Python/Problem/00287.Find the Duplicate Number.py
+++ SourceCode/Python/Problem/00287.Find the Duplicate Number.py
@@ -30,7 +30,7 @@ All the integers in nums appear only once except for precisely one integer which
 '''
 
 class Solution(object):
-    def findDuplicate(self, nums):
+    def findDuplicate_not_meet_space_limit(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -43,6 +43,40 @@ class Solution(object):
                 return i
 
 
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        '''
+        We use fast/slow ptr method in checking cycle of LinkList
+        Set two ptrs, slow one move 1 step, fast one 2 steps
+        They must meet during traversal
+        Then we set one of ptr to 0 to restart moving from start poing(usually fast ptr)
+        If this pty whcich restart moving from start point still be able to reach other pts
+        It means there is a cycle in LinkList, there is a duplicate number in list as welll
+
+        Limittion:
+        Integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+        1 <= n <= 105
+        nums.length == n + 1
+        1 <= nums[i] <= n
+        '''
+
+        slow = nums[0]
+        fast = nums[nums[0]]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+        fast = 0
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
 
 
 nums = [1,3,4,2,2]
