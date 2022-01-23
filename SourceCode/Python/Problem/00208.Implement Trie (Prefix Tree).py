163b75f4cdcaa4fba6db0fb142a749705554e1ee
roger <roger@macos.com>
2022-01-23 22:33:30 +0800

    Add topics

--- SourceCode/Python/Problem/00208.Implement Trie (Prefix Tree).py
+++ SourceCode/Python/Problem/00208.Implement Trie (Prefix Tree).py
@@ -1,4 +1,50 @@
 '''
+Level: Medium  Tag: [Trie]
+
+
+A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and
+retrieve keys in a dataset of strings.
+There are various applications of this data structure, such as autocomplete and spellchecker.
+
+Implement the Trie class:
+
+Trie() Initializes the trie object.
+void insert(String word) Inserts the string word into the trie.
+boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before),
+and false otherwise.
+boolean startsWith(String prefix) Returns true if there is a previously inserted string word
+that has the prefix prefix, and false otherwise.
+
+
+Example 1:
+
+Input
+["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+Output
+[null, null, true, false, true, null, true]
+
+Explanation
+Trie trie = new Trie();
+trie.insert("apple");
+trie.search("apple");   // return True
+trie.search("app");     // return False
+trie.startsWith("app"); // return True
+trie.insert("app");
+trie.search("app");     // return True
+
+
+Constraints:
+
+1 <= word.length, prefix.length <= 2000
+word and prefix consist only of lowercase English letters.
+At most 3 * 10^4 calls in total will be made to insert, search, and startsWith.
+
+
+
+========  Old Description  =========
+
+
 # Time:  O(n), per operation
 # Space: O(1)
 Implement a trie with insert, search, and startsWith methods.
@@ -71,6 +117,39 @@ class Trie(object):
             if curr is None:
                 return False
         return True
+
+
+class Trie2(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+
+
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
